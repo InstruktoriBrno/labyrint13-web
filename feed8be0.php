9f0656f11ae24fd27848e31ae90a3f7efa8ada68
Martin Kulich <kulda.zbraslav@gmail.com>
2020-05-23 12:39:56 +0000

    Initial commit

---
+++ feed8be0.php
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="FeedCreator 1.7.2-ppt DokuWiki" -->
+<?xml-stylesheet href="http://labyrint13.instruktori.cz/lib/exe/css.php?s=feed" type="text/css"?>
+<rdf:RDF
+    xmlns="http://purl.org/rss/1.0/"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+    xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <channel rdf:about="http://labyrint13.instruktori.cz/feed.php">
+        <title>Labyrint: cesta životem</title>
+        <description></description>
+        <link>http://labyrint13.instruktori.cz/</link>
+        <image rdf:resource="http://labyrint13.instruktori.cz/lib/tpl/prsnl10/images/favicon.ico" />
+       <dc:date>2019-01-05T15:12:59+01:00</dc:date>
+        <items>
+            <rdf:Seq>
+                <rdf:li rdf:resource="http://labyrint13.instruktori.cz/o_labyrintu?rev=1367701617&amp;do=diff"/>
+                <rdf:li rdf:resource="http://labyrint13.instruktori.cz/o_metode?rev=1370031245&amp;do=diff"/>
+                <rdf:li rdf:resource="http://labyrint13.instruktori.cz/od_koho?rev=1368695574&amp;do=diff"/>
+                <rdf:li rdf:resource="http://labyrint13.instruktori.cz/prakticke?rev=1371252251&amp;do=diff"/>
+                <rdf:li rdf:resource="http://labyrint13.instruktori.cz/prihlaska?rev=1376931291&amp;do=diff"/>
+                <rdf:li rdf:resource="http://labyrint13.instruktori.cz/prihlaska_pok?rev=1367187080&amp;do=diff"/>
+            </rdf:Seq>
+        </items>
+    </channel>
+    <image rdf:about="http://labyrint13.instruktori.cz/lib/tpl/prsnl10/images/favicon.ico">
+        <title>Labyrint: cesta životem</title>
+        <link>http://labyrint13.instruktori.cz/</link>
+        <url>http://labyrint13.instruktori.cz/lib/tpl/prsnl10/images/favicon.ico</url>
+    </image>
+    <item rdf:about="http://labyrint13.instruktori.cz/o_labyrintu?rev=1367701617&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2013-05-04T23:06:57+01:00</dc:date>
+        <title>o_labyrintu</title>
+        <link>http://labyrint13.instruktori.cz/o_labyrintu?rev=1367701617&amp;do=diff</link>
+        <description>Labyrint nabízí jistá tajemství, mnoho chodeb, za jejichž rohem se skrývá překvapení. Cesta je někdy přímá, jasná a jindy zas pěkně zašmodrchaná, s množstvím slepých uliček. Objevit v něm můžete radost, napětí, společnost druhých lidí, zábavu i poznání, sem tam možná i trochu toho zklamání, když nenajdete přesně to, co hledáte. Skrývá však i mnoho krás a třeba i drahocenných pokladů, které čekají na své nálezce. Labyrint je tak trochu jako náš život...…</description>
+    </item>
+    <item rdf:about="http://labyrint13.instruktori.cz/o_metode?rev=1370031245&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2013-05-31T22:14:05+01:00</dc:date>
+        <title>o_metode</title>
+        <link>http://labyrint13.instruktori.cz/o_metode?rev=1370031245&amp;do=diff</link>
+        <description>Metoda, které se v Čechách nejčastěji říká zážitková pedagogika, je jeden ze způsobů jak realizovat vzdělávací a seberozvojové kurzy. Zážitkové kurzy (dále ZK) bývají nejčastěji zacíleny na dospívající a mladé dospělé. Vhodná je však pro osoby jakéhokoliv věku, které mají zájem o rozvoj a učení se možná trochu netradičními metodami.</description>
+    </item>
+    <item rdf:about="http://labyrint13.instruktori.cz/od_koho?rev=1368695574&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2013-05-16T11:12:54+01:00</dc:date>
+        <title>od_koho</title>
+        <link>http://labyrint13.instruktori.cz/od_koho?rev=1368695574&amp;do=diff</link>
+        <description>Labyrint pro vás pod hlavičkou Instruktoři Brno pořádají: 
+
+[Divi]
+
+DIVI (IS Brno)
+
+Psycholog/psychoterapeut, občasný učitel i věčný student, sportovec i povaleč, především však člověk. 
+Byl zplozen zjara, narodil se v neděli a vyrůstal živen nadějí, sny a láskou, radostí i žalem, vírou i pochybami. Dožil se úctyhodného věku devětadvaceti let a snaží se žít tak, aby nikomu neškodil, nedělal druhým věci, které sám nechce, byl užitečný. Rád tvoří, rád hraje, si hraje, se hraje, občas rád i jen tak…</description>
+    </item>
+    <item rdf:about="http://labyrint13.instruktori.cz/prakticke?rev=1371252251&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2013-06-15T01:24:11+01:00</dc:date>
+        <title>prakticke</title>
+        <link>http://labyrint13.instruktori.cz/prakticke?rev=1371252251&amp;do=diff</link>
+        <description>*  Termín: 10.8. - 18.8.2013
+	*  Místo: Pelhřimovsko
+	*  Účastníci: 16 účastníků ve věku 21 a více
+	*  Cena: cca 4600 korun
+	*  Kontakt: labyrint(o)bespin.cz (Janina)
+
+O kurzu
+
+Naší snahou je nabídnout nabitý, avšak vyvážený a pestrý program pro tělo i duši, náročný tak aby pro Tebe mohly být některé aktivity výzvou, jiné odpočinkem. Možná se během kurzu dotkneme některých osobních témat a přesto, že je v týmu několik psychologů, nejedná se o skupinovou ani individuální psychoterapii.…</description>
+    </item>
+    <item rdf:about="http://labyrint13.instruktori.cz/prihlaska?rev=1376931291&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2013-08-19T18:54:51+01:00</dc:date>
+        <title>prihlaska</title>
+        <link>http://labyrint13.instruktori.cz/prihlaska?rev=1376931291&amp;do=diff</link>
+        <description>Již se není kam přihlásit, ale určitě si vybereš v kalendáři na Instruktorů Brno!</description>
+    </item>
+    <item rdf:about="http://labyrint13.instruktori.cz/prihlaska_pok?rev=1367187080&amp;do=diff">
+        <dc:format>text/html</dc:format>
+        <dc:date>2013-04-29T00:11:20+01:00</dc:date>
+        <title>prihlaska_pok</title>
+        <link>http://labyrint13.instruktori.cz/prihlaska_pok?rev=1367187080&amp;do=diff</link>
+        <description>----------
+
+
+
+Chceš jet s námi? Vyplň prosím následující krátký kontaktní formulář, do pár dní se ti emailem ozveme s dalšími informacemi!
+
+Na viděnou na Labyrintu!</description>
+    </item>
+</rdf:RDF>
---
+++ lib/exe/opensearch.php
@@ -0,0 +1,7 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+  <ShortName>Labyrint: cesta životem</ShortName>
+  <Image width="16" height="16" type="image/x-icon">http://labyrint13.instruktori.cz/favicon.ico</Image>
+  <Url type="text/html" template="http://labyrint13.instruktori.cz/doku.php?do=search&amp;id={searchTerms}" />
+  <Url type="application/x-suggestions+json" template="http://labyrint13.instruktori.cz/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
+</OpenSearchDescription>
